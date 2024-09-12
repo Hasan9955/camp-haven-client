5355d6f2ce3057a33642bbfb276f95682a45bc43
Hasan9955 <hasanbinali7556@gmail.com>
2024-09-12 06:44:05 +0600

    complete setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() { 
+
+  return (
+    <div className='text-center font-bold text-5xl'> 
+      <h1>Hasan Bin Ali</h1> 
+    </div>
+  )
+}
+
+export default App
